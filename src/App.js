cf158a087c40f65d76e21e9b46f41d54fcefd030
Kirk Hansen <kirk.hansen@pioneer.com>
2021-02-25 11:52:51 -0600

    past cors issue...
    onto server 500 issue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import { ApolloClient, ApolloLink, ApolloProvider, HttpLink, InMemoryCache, concat} from '@apollo/client';
+import { createHttpLink } from "apollo-link-http";
 import './App.css';
 import { HyveeVaccines } from "./Hyvee";
 
-const _LINK = new HttpLink({uri: "https://www.hy-vee.com/my-pharmacy/api/graphql"});
-
-const _CORS = new ApolloLink((operation, forward) => {
-  operation.setContext({
+// const _LINK = new HttpLink({uri: "https://www.hy-vee.com/my-pharmacy/api/graphql"});
+//
+// const _CORS = new ApolloLink((operation, forward) => {
+//   operation.setContext({
+//     headers: {
+//       "Access-Control-Allow-Origin": "*",
+//     }
+//   });
+//   return forward(operation);
+// });
+//
+const _LINK = createHttpLink({
+    uri: "https://www.hy-vee.com/my-pharmacy/api/graphql",
     headers: {
-      "Access-Control-Allow-Origin": "*",
-    }
-  });
-  return forward(operation);
+        "Access-Control-Allow-Origin": "*",
+    },
+    fetchOptions: {
+        mode: "no-cors",
+    },
 });
 
 
 
 const HYVEE_APOLLO_CLIENT = new ApolloClient({
   cache: new InMemoryCache(),
-  link: concat(_CORS, _LINK),
+  link: _LINK,
 });
 
 function App() {
